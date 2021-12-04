b168c3136ea057006da84823294ecd24ac9de07c
Eliseo Falcon <eliseonop@gmail.com>
2021-12-03 21:02:11 -0500

    update

--- src/views/Contacto.js
+++ src/views/Contacto.js
@@ -1,14 +1,26 @@
 import "../css/contacto.css"
+import axios from 'axios'
 
 export default function Contacto() {
     return (
-        <div className="container text-center" id="contactos">
+        <div className="text-center" id="contactos">
             <div className="container" id="containercontacto">
                 <h1>Mantente en contacto</h1>
                 <p>Regístrate para recibir noticias sobre nuevos lanzamientos y promociones esclusivas.</p>
                 <inputcontacto class="input-group">
-                    <input type="text" class="form-control" placeholder="Ingrese su email" />
+                    <input 
+                        type="email" 
+                        id="meailform"
+                        class="form-control" 
+                        placeholder="Ingrese su email" 
+                    />
                 </inputcontacto>
+                <button
+                  type="submit"
+                  className="btn btn-outline-light   mt-3"
+                >
+                  Suscribirse
+                </button>                  
             </div>
         </div>
     )
